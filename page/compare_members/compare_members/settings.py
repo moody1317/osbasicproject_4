2ed727ead7241275686ed26c3ededd45166cfb62
Hanseoyun312 <hansy68922@naver.com>
2025-06-01 05:41:08 +0900

    Render 배포 설정: render.yaml, requirements.txt, settings.py 수정

--- page/compare_members/compare_members/settings.py
+++ page/compare_members/compare_members/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mw-48-f1nyvtv*=jk^#afm$&jgd#0*=zm!wi@wb-5)in^w%w25
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 
 # Application definition
