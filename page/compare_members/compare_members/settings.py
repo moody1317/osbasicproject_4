1a0b7fb492cbd62600e909fb150bbbb884ea3bd4
Hanseoyun312 <hansy68922@naver.com>
2025-05-31 15:11:09 +0900

    Add Render deploy config

--- page/compare_members/compare_members/settings.py
+++ page/compare_members/compare_members/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mw-48-f1nyvtv*=jk^#afm$&jgd#0*=zm!wi@wb-5)in^w%w25
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
